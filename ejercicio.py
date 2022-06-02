f5b8bc3dcbbc8613d826cfeaa3a0468c07432fc3
Ramsespf <ramses.fagundo@gmail.com>
2022-06-02 16:52:39 -0400

    First commit

--- ejercicio.py
+++ ejercicio.py
@@ -1,4 +1,5 @@
-peso = input("Ingrese su peso en Kg: ")
-estatura = input ("Ingrese su estatura en metros: ")
-imc = float(peso) / (float(estatura) ** 2)
-print(f"Tu índice de masa corporal es {round(imc,2)} ")
+edad = int(input("Diga su edad: "))
+if edad >= 18:
+    print("Usted es mayor de edad")
+else:
+    print("Usted es menor de edad")
