ee4bed0cfbce7c428f388325cdc810a0c5187bce
Ramsespf <ramses.fagundo@gmail.com>
2022-06-07 09:33:13 -0400

    first commit Ejercicio2

---
+++ Tema5/ejercicio2.py
@@ -0,0 +1,10 @@
+import math
+
+def primo(numero):
+    multiplo = math.factorial(numero - 1) + 1
+    if multiplo % numero == 0:
+        print(f'{numero} es un numero primo')
+    else:
+        print(f'{numero} no es un numero primo')
+
+primo(113)
